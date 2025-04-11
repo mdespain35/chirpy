7a54f7d31fa6482ff12de08d18ae73747e94b53f
MICHAEL DE SPAIN <mdespain35@gmail.com>
2025-04-11 14:57:15 -0700

    Added visit tracking

--- main.go
+++ main.go
@@ -3,13 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	servMux := http.NewServeMux()
-	servMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+	servMux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	servMux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 	servMux.HandleFunc("/healthz", healthHandler)
+	servMux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	servMux.HandleFunc("/reset", apiCfg.resetMetricsHandler)
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: servMux,
@@ -21,6 +39,19 @@ func main() {
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
-	w.Write([]byte{'O', 'K'})
+	w.Write([]byte("OK"))
 
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("Hits: " + strconv.Itoa(int(cfg.fileserverHits.Load()))))
+}
+
+func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Swap(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("Metrics successfully reset."))
 }
